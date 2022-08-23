7f009b5d5718ecbba94fb76679a97e7605d6d774
ren831 <rene83120@gmail.com>
2022-08-22 18:14:55 -0700

    commit tree 03c92ebb72b8612b6089e47fb5836f3b3e31ef52

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,10 @@
-
+class Book
+  attr_accessor :title, :genre, :author, :page_count
+  def initialize(title )
+    @title = title
+  end
+  def turn_page
+   puts "Flipping the page...wow, you read fast!"
+  end
+end
 
